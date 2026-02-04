6c86912e7d8035c90e2c1397e7666ebd0149ecc6
Reynaldiii <reynaldi191818@gmail.com>
2026-02-04 08:34:36 +0700

    feat: add supabase as database

--- backend/settings.py
+++ backend/settings.py
@@ -81,8 +81,12 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',         
+        'USER': 'postgres.xmsffgwcjeequpqjhvqi',         
+        'PASSWORD': 'SonixAlwaysSonix', 
+        'HOST': 'aws-1-ap-southeast-2.pooler.supabase.com', 
+        'PORT': '6543',
     }
 }
 
