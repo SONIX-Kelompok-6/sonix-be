373c04fc234806297210098e5cfb0f9fa34a966f
wilsonPhan02 <wilsonphan002@gmail.com>
2026-02-15 23:56:59 +0700

    fix settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -86,13 +86,30 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
-DATABASES = {
-    'default': dj_database_url.config(
-        # Default: Pake SQLite di local biar aman & cepet
-        default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
-        conn_max_age=600
-    )
-}
+DATABASE_URL = os.environ.get('DATABASE_URL')
+
+if DATABASE_URL:
+    # Kalau ketemu DATABASE_URL (baik di Railway atau di terminal laptop)
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+else:
+    # Pilihan terakhir kalau emang gak ada koneksi internet/env
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
+# DATABASES = {
+#     'default': dj_database_url.config(
+#         # Default: Pake SQLite di local biar aman & cepet
+#         default='sqlite:///' + os.path.join(BASE_DIR, 'db.sqlite3'),
+#         conn_max_age=600
+#     )
+# }
+
 
 
 # Password validation
