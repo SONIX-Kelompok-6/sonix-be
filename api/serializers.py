eb7aa3996103fe4b0b86d81cc69a26ec15348c9f
Reynaldiii <reynaldi191818@gmail.com>
2026-02-16 19:10:37 +0700

    fix: update serializers

--- api/serializers.py
+++ api/serializers.py
@@ -1,17 +1,19 @@
 from rest_framework import serializers
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model # Cara aman ambil model User
 from .models import UserProfile, Shoe
+
+# Ambil model User yang aktif (Custom User kita)
+User = get_user_model()
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'email', 'password']
+        fields = ['id', 'username', 'email', 'password']
         extra_kwargs = {'password': {'write_only': True}}
 
     def create(self, validated_data):
-        # Kita pakai 'email' sebagai username juga biar simpel
         user = User.objects.create_user(
-            username=validated_data['email'], 
+            username=validated_data['username'], 
             email=validated_data['email'],
             password=validated_data['password']
         )
@@ -23,12 +25,12 @@ class UserProfileSerializer(serializers.ModelSerializer):
         fields = ['foot_width', 'arch_type', 'uses_orthotics']
 
 class UserDetailSerializer(serializers.ModelSerializer):
-    # Kita masukkan profile ke dalam user
+    # Gabungkan data profile ke dalam detail user
     profile = UserProfileSerializer(read_only=True)
 
     class Meta:
         model = User
-        fields = ['username', 'email', 'date_joined', 'profile']
+        fields = ['id', 'username', 'email', 'date_joined', 'profile']
 
 class ShoeSerializer(serializers.ModelSerializer):
     class Meta:
