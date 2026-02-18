cb6cfbd6787952b820b76c24a780c3ef1b768fab
Reynaldiii <reynaldi191818@gmail.com>
2026-02-19 04:50:49 +0700

    fix: fix error

--- backend/settings.py
+++ backend/settings.py
@@ -21,7 +21,7 @@ import dj_database_url
 # load_dotenv()
 
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # Quick-start development settings - unsuitable for production
